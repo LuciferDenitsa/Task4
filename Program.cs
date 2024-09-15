04a25593ee3a98a453ddefb1f02ec9f03e9322ff
Daniil <rychenkov16@yandex.ru>
2024-09-15 17:31:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace Task4;
+
+class Program
+{
+    public static void Main()
+    {
+        Reader(null);
+    }
+
+    private static async void Reader(string? path)
+    {
+        var student = new List<Student>();
+        var p = path ?? "Students.dat";
+
+        using (var s = new BinaryReader(File.Open(p, FileMode.Open), Encoding.UTF8))
+        {
+            while (s.PeekChar() > -1)
+            {
+                var ss = s.Read();
+                string dd = s.ReadString();
+                Console.WriteLine($"{ss}  {dd}");
+
+            }
+        }
+
+        Console.ReadKey();
+
+    }
+}
+}
